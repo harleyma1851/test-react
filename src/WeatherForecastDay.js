119c11a76c9225efc2dd831b26765093306e2403
Michelle Harley <harleyma1851@gmail.com>
2023-02-02 14:44:33 -0600

    Update WeatherForecastDay.js
    added day info. Getting error in terminal, will check with instructor since I have tried trouble shooting on my own.

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,17 +2,35 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temp.max);
+    return `${temperature}°`;
+  }
+  function minTemperature() {
+    let temperature = Math.round(props.data.temp.min);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+
   return (
     <div>
       <div className="WeatherForecast-day">{props.data.dt}</div>
       <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="WeatherForecast-temperature">
         <span className="WeatherForecast-temperature-max">
-          {Math.round(forecast[0].temp.max)}°
+          {maxTemperature()}
         </span>{" "}
         |{" "}
         <span className="WeatherForecast-temperature-min">
-          {Math.round(forecast[0].temp.min)}°
+          {minTemperature()}
         </span>
       </div>
     </div>
